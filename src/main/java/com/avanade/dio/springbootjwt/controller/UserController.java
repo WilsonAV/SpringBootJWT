842e00f2eac4693d8a772dd294c9bb147d9b19cf
Wilson Nunes <wilson-avn1@hotmail.com>
2021-07-11 21:01:12 -0300

    JWT Authentication Spring Boot

---
+++ src/main/java/com/avanade/dio/springbootjwt/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.avanade.dio.springbootjwt.controller;
+
+import com.avanade.dio.springbootjwt.data.UserData;
+import com.avanade.dio.springbootjwt.service.UserDetailsServiceImpl;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public UserController(UserDetailsServiceImpl userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @RequestMapping("/all-users")
+    public List<UserData> listAllUsers(){
+        return userDetailsService.listUsers();
+    }
+}
